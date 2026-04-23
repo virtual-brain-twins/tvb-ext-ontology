3548722aad4de64ec2c0d41003b77e7dede9a4e7
adrianciu <adrian.ciu@codemart.ro>
2026-04-23 16:57:09 +0300

    VT-111: add tests for different experiment configurations

--- tvb_ext_ontology/tests/test_run_models.py
+++ tvb_ext_ontology/tests/test_run_models.py
@@ -1,28 +1,31 @@
-# import unittest
-# from tvbo.api import ontology_api
-# from tvbo.knowledge.ontology import get_models
-#
-#
-# class TestOntologyAPI(unittest.TestCase):
-#
-#     def test_simulation_experiment(self):
-#         models = get_models().keys()  # Fetch actual models
-#         api = ontology_api.OntologyAPI()  # Initialize real API
-#
-#         for m in models:
-#             metadata = {
-#                 "model": {"label": m, "parameters": {}},
-#                 "connectivity": {
-#                     "parcellation": {"atlas": {"name": "Destrieux"}},
-#                     "tractogram": {"label": "PPMI85"},
-#                 },
-#                 "coupling": {"label": "Linear"},
-#                 "integration": {"method": "Heun", "noise": None},
-#             }
-#             # Configure and run the simulation experiment
-#             api.configure_simulation_experiment(metadata)
-#             api.experiment.run(simulation_length=10)
-#
-#
-# if __name__ == "__main__":
-#     unittest.main()
+import pytest
+
+from tvbo.api import ontology_api
+from tvbo.ontology.owl import get_models
+
+
+MODELS = sorted(get_models().keys())
+
+
+@pytest.mark.parametrize("model", MODELS)
+def test_simulation_experiment(model):
+    api = ontology_api.OntologyAPI()
+
+    metadata = {
+        "dynamics": model,
+        "coupling": "Linear",
+        "network": {
+            "parcellation": {
+                "atlas": {
+                    "name": "DesikanKilliany",
+                }
+            },
+            "tractogram": {
+                "name": "dTOR",
+            },
+        },
+        "integration": {"method": "Heun", "noise": None},
+    }
+
+    api.configure_simulation_experiment(metadata)
+    api.experiment.run(simulation_length=10)
